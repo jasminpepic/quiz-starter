e222f0373dd70bc7c13ed8c0ff81303cbf596ff2
unknown <pepicjasmin@gmail.com>
2022-10-19 21:05:25 +0200

    test1

--- quiz.js
+++ quiz.js
@@ -1,1 +1,25 @@
+// console.log(questions);
+
+// let a = 5;
+// let b = 6;
+// let c = 0;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(Math.floor(Math.random() * 10));
+
+function shuffle(questions) {
+  for (let i = 0; i < 10000; i++) {
+    const randomNumber1 = Math.floor(Math.random() * questions.length);
+    const randomNumber2 = Math.floor(Math.random() * questions.length);
+
+    const temp = questions[randomNumber1];
+    questions[randomNumber1] = questions[randomNumber2];
+    questions[randomNumber2] = temp;
+  }
+}
+
+shuffle(questions);
 console.log(questions);
